02677d60cb6b096231e0bf35348193a20deeced4
henri <henri.me@skiff.com>
2024-09-16 18:14:30 +0200

    tried to fix the flatlist problem

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Page from "@/components/page";
 import { cn } from "@/utils/cn";
 import { ScrollView, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 const data = [
   { id: 1, title: "Home" },
   { id: 2, title: "About" },
@@ -22,16 +23,32 @@ export default function HomeScreen() {
     "orange",
   ]
   return (
-    <Page className="flex flex-col" header>
+    <Page className="flex flex-col space-y-2" header>
       <Header />
-      <ScrollView horizontal>
+      <ScrollView className="my-2" horizontal>
         {data.map((item) => (
-          <View key={item.id} className={cn("p-3 rounded-md h-32 w-64 mx-2")} style={{ backgroundColor: colors[item.id % colors.length] }}>
-            <Text className="text-sm text-[#333333]">Categorys</Text>
+          <View
+            key={item.id}
+            className={cn("p-3 rounded-md h-32 w-64 mx-2")}
+            style={{ backgroundColor: colors[item.id % colors.length] }}
+          >
+            <Text className="text-sm text-[#333333]">Category</Text>
             <Text className="text-2xl font-medium m-0 p-0">{item.title}</Text>
           </View>
         ))}
       </ScrollView>
+
+      {/* FlatList should be rendered outside of any ScrollView */}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View className={cn("p-3 w-full h-24 border-t border-t-[#333333]")}>
+            <Text className="text-sm text-[#333333]">Category</Text>
+            <Text className="text-2xl font-medium m-0 p-0">{item.title}</Text>
+          </View>
+        )}
+      />
     </Page>
   );
 }
